378f748ed86e7d54440ed0534e6cf1105337005e
dailyworker <seansin@cbnu.ac.kr>
2019-08-09 14:25:02 +0900

    SpringBootApplication에 관해

--- src/main/java/it/topping/demoSpring/DemoApplication.java
+++ src/main/java/it/topping/demoSpring/DemoApplication.java
@@ -1,6 +1,7 @@
 package it.topping.demoSpring;
 
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,19 +13,13 @@ import java.util.Arrays;
  * Blog : http://dailyworker.github.io
  * Github : http://github.com/dailyworker
  */
+// ApplicationContext 등록
+@SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args){
-        // Java 설정파일 ApplicationContext 등록
-        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-
-        // XML 설정파일 ApplicationContext 등록
-        // ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        System.out.println(Arrays.toString(beanDefinitionNames));
-
-        BookService bookService = (BookService) context.getBean("bookService");
-        // 의존성 주입 확인
-        System.out.println(bookService.bookRepository != null);
+        /**
+         * 스프링 부트 기반에서는 @SpringBootApplication 애노테이션이 붙였을 때
+         * @Configuration과 @ComponentScan의 기능이 이미 담겨있으므로 ApplicationContext를 따로 사용할 일은 드물다.
+         */
     }
 }
