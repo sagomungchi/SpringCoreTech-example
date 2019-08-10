c7343bb7c8db335a4e61d0b1645023226bf167b5
dailyworker <seansin@cbnu.ac.kr>
2019-08-10 17:24:15 +0900

    Setter AutoWired

--- src/main/java/it/topping/demoSpring/BookService.java
+++ src/main/java/it/topping/demoSpring/BookService.java
@@ -1,5 +1,6 @@
 package it.topping.demoSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +12,14 @@ import org.springframework.stereotype.Service;
 public class BookService {
     BookRepository bookRepository;
 
+    /**
+     * 생성자 의존성 주입은 빈을 만들다가 다른 의존성을 못찾아서 실패
+     * Setter? -> BookService 자체를 만들 수 있지 않는가?
+     * -> @Autowired라는 것을 만나서 의존성 주입을 하고자해서 실패
+     * -> 따라서 인터페이스만 만들고자 하는 경우는 의존성을 옵션으로 주면된다.
+     *
+     */
+    @Autowired(required = false)
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
--- src/main/java/it/topping/demoSpring/DemoApplication.java
+++ src/main/java/it/topping/demoSpring/DemoApplication.java
@@ -1,12 +1,8 @@
 package it.topping.demoSpring;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Arrays;
 
 /**
  * Created by seansin@cbnu.ac.kr.com on 09/08/2019
@@ -17,9 +13,6 @@ import java.util.Arrays;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args){
-        /**
-         * 스프링 부트 기반에서는 @SpringBootApplication 애노테이션이 붙였을 때
-         * @Configuration과 @ComponentScan의 기능이 이미 담겨있으므로 ApplicationContext를 따로 사용할 일은 드물다.
-         */
+        SpringApplication.run(DemoApplication.class);
     }
 }
