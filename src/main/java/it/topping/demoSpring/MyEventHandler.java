61cbd40833be00c69f05adc1d00d25ae797b86ee
dailyworker <seansin@cbnu.ac.kr>
2019-08-10 23:22:45 +0900

    ApplicationContext - ApplicationEventPublisher ContextRefreshedEvent와 ContextClosedEvent

--- src/main/java/it/topping/demoSpring/MyEventHandler.java
+++ src/main/java/it/topping/demoSpring/MyEventHandler.java
@@ -1,5 +1,7 @@
 package it.topping.demoSpring;
 
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -17,5 +19,20 @@ public class MyEventHandler {
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 받았다. 데이터는 " + event.getData());
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextRefreshedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextRefreshedEvent");
+    }
+
+
+    @EventListener
+    @Async
+    public void handle(ContextClosedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextClosedEvent");
     }
 }
