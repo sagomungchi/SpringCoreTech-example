766b0f9e6006fb5cd82b96212e5989d62c170a0b
dailyworker <seansin@cbnu.ac.kr>
2019-08-10 21:25:26 +0900

    Thread-Safe

--- src/main/java/it/topping/demoSpring/Proto.java
+++ src/main/java/it/topping/demoSpring/Proto.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
  * Blog : http://dailyworker.github.io
  * Github : http://github.com/dailyworker
  */
+
+// 클래스 기반의 프록시로 감싸라!
 @Component @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Proto {
 
--- src/main/java/it/topping/demoSpring/Single.java
+++ src/main/java/it/topping/demoSpring/Single.java
@@ -10,13 +10,12 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Single {
-    /**
-     * SingleTone Scope의 빈이 ProtoType Scope의 빈을 사용할 경우
-     * ProtoType의 빈을 사용하려고 프로터피를 지정할 때,
-     * ProtoType의 빈은 계속 생성되기때문에 프로퍼티가 변경이 안된다.
-     */
+
     @Autowired
     Proto proto;
+
+    // Thread-Safe X
+    int value = 0;
 
     public Proto getProto() {
         return proto;
